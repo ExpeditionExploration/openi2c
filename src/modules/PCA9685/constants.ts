b8ad3e22728d477cf214adb7819cca4da8c61efb
Mikko Kujala <m.kujala@live.com>
2025-04-22 06:39:13 +0300

    prescale address corrected.

--- src/modules/PCA9685/constants.ts
+++ src/modules/PCA9685/constants.ts
@@ -1,5 +1,5 @@
 export const MODE1 = 0x00;
-export const PRESCALE = 0x01;
+export const PRESCALE = 0xFE;
 export const LED0_ON_L = 0x06;
 export const LED0_ON_H = 0x07;
 export const LED0_OFF_L = 0x08;

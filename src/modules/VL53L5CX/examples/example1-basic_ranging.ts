18a880b5c20bdf34e27f41b804adbd6d1df93ca2
Mikko Kujala <m.kujala@live.com>
2025-04-30 18:42:02 +0300

    TypeScriptify the interface

--- src/modules/VL53L5CX/examples/example1-basic_ranging.ts
+++ src/modules/VL53L5CX/examples/example1-basic_ranging.ts
@@ -1,28 +1,22 @@
 import { stdout } from "process";
-import { vl53l5cx } from "..";
+import { ScanData, VL53L5CX } from "..";
 import { sleep } from "../../../utils";
 
 function example1() {
-    try {
-        vl53l5cx.vl53l5cx_comms_init(0);
-        vl53l5cx.vl53l5cx_is_alive(0);
-        vl53l5cx.vl53l5cx_start_ranging(0);
-        for (let i = 0; i < 10; i++) {
-            while (vl53l5cx.vl53l5cx_check_data_ready(0) == false) {
-                const sab = new SharedArrayBuffer(4);
-                const int32 = new Int32Array(sab);
-                Atomics.wait(int32, 0, 0, 5); // Sleep for 5ms
-            }
-            const data = vl53l5cx.vl53l5cx_get_ranging_data(0);
-            console.log(`Chip temperature: ${data.chipTempC}`);
-            for (let zone of data.scanZones) {
-                if (zone)
-                    console.log(zone)
-            }
+    const sensor = new VL53L5CX(1, {address: 0x52});
+    sensor.startRanging();
+    for (let i = 0; i < 10; i++) {
+        while(sensor.checkDataReady() == false) {
+            const sab = new SharedArrayBuffer(4);
+            const int32 = new Int32Array(sab);
+            Atomics.wait(int32, 0, 0, 5); // Sleep for 5ms
         }
-        vl53l5cx.vl53l5cx_stop_ranging(0);
-    } catch (e) {
-        console.log(e);
+        const data = sensor.getRangingData();
+        console.log(`Chip temperature: ${data.chipTempC}C`);
+        for (let zone of data.scanZones) {
+            if (zone)  // TODO: Change C code to make arr len reflect zone count
+                console.log(zone)
+        }
     }
 }
 
